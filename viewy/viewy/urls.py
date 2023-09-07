b523112b59ff827fc242edce35b862f5607c4a17
ryoji-test <ryoji@icloud.com>
2023-09-07 15:14:42 +0900

    デバックツールバーの設定

--- viewy/viewy/urls.py
+++ viewy/viewy/urls.py
@@ -21,7 +21,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += [
         path('admin/', admin.site.urls),
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
