e59817edcee3ad5c7b8eb0c3a2d947488c796d70
ryoji-test <ryoji@icloud.com>
2023-09-28 15:12:26 +0900

    is_hiddenを50回報告でTrueにし、親クラス継承

--- viewy/static/js/mangapage_count.js
+++ viewy/static/js/mangapage_count.js
@@ -1,30 +1,37 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   function handleBookScroll(event) {
     const book = event.target;
     const mangaPages = book.querySelectorAll('.manga-page');
     const currentPageDisplay = book.parentNode.querySelector('#current-page');
+    const totalPageDisplay = book.parentNode.querySelector('.total-page');
     const pageWidth = mangaPages[0].offsetWidth;
     let currentPage = 1 - Math.floor(book.scrollLeft / pageWidth);
+    const maxPages = parseInt(totalPageDisplay.textContent, 10);
+
+    // もしカレントページが最大ページ数を超えていれば、カレントページを最大ページ数にセット
+    if (currentPage > maxPages) {
+      currentPage = maxPages;
+    }
 
     currentPageDisplay.textContent = currentPage;
   }
 
   // 初期に存在する.book要素に対してイベントリスナーを設定
   const books = document.querySelectorAll('.book');
-  books.forEach(function(book) {
+  books.forEach(function (book) {
     book.addEventListener('scroll', handleBookScroll);
   });
 
   // .screen要素の変更を監視して、新しい.book要素に対してイベントリスナーを設定
   const screen = document.querySelector('.screen');
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      mutation.addedNodes.forEach(function(addedNode) {
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      mutation.addedNodes.forEach(function (addedNode) {
         if (addedNode.nodeType === Node.ELEMENT_NODE) {
           const books = addedNode.querySelectorAll('.book');
-          books.forEach(function(book) {
+          books.forEach(function (book) {
             book.addEventListener('scroll', handleBookScroll);
           });
         }
