14b6e96b01e6d9f7141af0ee34e9f77d0df68b6f
ryoji-test <ryoji@icloud.com>
2023-10-08 14:13:31 +0900

    adminでのPostsを軽くする

--- viewy/posts/admin.py
+++ viewy/posts/admin.py
@@ -3,6 +3,11 @@ from .models import (
   Posts, Visuals, Videos, Favorites, Report, Ads, KanjiHiraganaSet, WideAds, ViewDurations, TomsTalk
 )
 
+class PostsAdmin(admin.ModelAdmin):
+    raw_id_fields = ('poster',)  # 大量の関連オブジェクトがあるフィールドを指定
+
+admin.site.register(Posts, PostsAdmin)
+
 admin.site.register(
-  [Posts,Visuals,Videos,Favorites,Report,Ads,KanjiHiraganaSet, WideAds, ViewDurations, TomsTalk]
+  [Visuals,Videos,Favorites,Report,Ads,KanjiHiraganaSet, WideAds, ViewDurations, TomsTalk]
 )
