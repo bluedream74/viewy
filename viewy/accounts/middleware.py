f3455c7623ec9214ef80a2ce46064ad8fb7803d0
ryoji-test <ryoji@icloud.com>
2023-10-12 17:44:15 +0900

    for_invitedなどをR18チェックから除く

--- viewy/accounts/middleware.py
+++ viewy/accounts/middleware.py
@@ -15,12 +15,23 @@ class AgeVerificationMiddleware:
         if request.path.startswith(settings.STATIC_URL) or request.path.startswith(settings.MEDIA_URL):
             return self.get_response(request)
 
-        # 現在のページが年齢確認ページではない場合のみリダイレクト処理を行う
-        if request.path != age_verification_url and not request.COOKIES.get('is_over_18'):
-            # nextパラメータに現在のページのパスを設定
-            query_string = urlencode({'next': request.path})
-            redirect_url = age_verification_url + '?' + query_string
-            return redirect(redirect_url)
+        # 除外したいURLのリスト
+        excluded_urls = [
+            "/for_invited/",
+            "/guide/",
+            "/for_advertiser/",
+            "/set_meeting/",
+            "/set_meeting_success/",
+        ]
+
+        # 現在のページが除外したいURLのいずれでもない場合のみリダイレクト処理を行う
+        if not any(request.path.startswith(url) for url in excluded_urls):
+            # 現在のページが年齢確認ページではない場合のみリダイレクト処理を行う
+            if request.path != age_verification_url and not request.COOKIES.get('is_over_18'):
+                # nextパラメータに現在のページのパスを設定
+                query_string = urlencode({'next': request.path})
+                redirect_url = age_verification_url + '?' + query_string
+                return redirect(redirect_url)
 
         response = self.get_response(request)
         return response
