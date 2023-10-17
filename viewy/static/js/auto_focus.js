e9980eb68ece9c0fa88556d410af435f71d2cfda
ryoji-test <ryoji@icloud.com>
2023-10-17 11:37:53 +0900

    contentを自動で非表示に、ふぁいる分割

--- viewy/static/js/auto_focus.js
+++ viewy/static/js/auto_focus.js
@@ -2,101 +2,17 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  function toggleVisibility(contentElement, labelElement) {
-    const input = labelElement ? labelElement.querySelector('.hide-input') : null;
-
-    if (contentElement.style.opacity === '0') {
-      contentElement.style.opacity = '1';
-      contentElement.style.pointerEvents = 'auto';
-      if (labelElement) {
-        labelElement.classList.replace('fa-angle-up', 'fa-angle-down');
-        if (input) input.checked = false;
-      }
-    } else {
-      contentElement.style.opacity = '0';
-      contentElement.style.pointerEvents = 'none';
-      if (labelElement) {
-        labelElement.classList.replace('fa-angle-down', 'fa-angle-up');
-        if (input) input.checked = true;
-      }
-    }
-  }
-  // 画面上のクリックイベントを処理する関数
-  document.addEventListener('click', function (e) {
-    const content = e.target.closest('.content');
-    const label = e.target.classList.contains('hide-label') ? e.target : null;
-
-    if (content) {
-      e.stopPropagation();
-      return;
-    }
-
-    if (label) {
-      const contentElement = label.previousElementSibling;
-      toggleVisibility(contentElement, label);
-    }
-  });
-
-  // 特定の要素がクリックされたときに実行される関数
-  function handleClick(event) {
-
-    const element = event.currentTarget;
-  
-    if (element.dataset.isAdvertisement === 'True' || event.target.closest('[data-is-advertisement="True"]')) {
-      console.log("Advertisement detected. Content will not be hidden on click.");
-      return;
-    }
-    if (event.target.closest('.content')) {
-      return;
-    }
-    const contentElement = element.querySelector('.content');
-    const labelElement = element.querySelector('.hide-label');
-
-    if (element.matches('.post') && !element.querySelector('.book')) {
-      return;
-    }
-
-    if (contentElement) toggleVisibility(contentElement, labelElement);
-  }
-  // 新しい要素を監視対象として追加する関数
   function observeTargets(newTargets) {
     newTargets.forEach(target => {
-      console.log("Observing target: ", target);
       observer.observe(target);
-
-      console.log("Advertisement attribute of the observed target: ", target.dataset.isAdvertisement);
-
-      if (target.matches('.book, .post') || target.querySelector('.book, .post')) {
-        target.addEventListener('click', handleClick);
-      }
     });
   }
-  // 交差を検出したときに実行される関数
+
   function isactive(entries) {
     entries.forEach(entry => {
-      const element = entry.target;
       if (entry.isIntersecting) {
-        element.setAttribute("tabindex", 0);
-        element.focus();
-
-        const contentElement = element.querySelector('.content');
-        const labelElement = element.querySelector('.hide-label');
-
-        // ここで広告かどうかをチェック
-        if (element.dataset.isAdvertisement === 'True') {
-          console.log("Advertisement detected. Content will not be hidden automatically.");
-          return;
-        }
-
-        if ((element.querySelector('.book') || element.classList.contains('book')) && contentElement) {
-          setTimeout(() => {
-            if (contentElement.style.opacity !== '0') {
-              contentElement.style.transition = "opacity 0.3s ease";
-              toggleVisibility(contentElement, labelElement);
-            }
-          }, 3000);
-        }
-        observer.unobserve(element);
+        entry.target.setAttribute("tabindex", 0);
+        entry.target.focus();
       }
     });
   }
@@ -117,15 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
       if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
         mutation.addedNodes.forEach(node => {
           if (node instanceof HTMLElement) {
-            setTimeout(() => {
-              let newTargets = Array.from(node.querySelectorAll('.book, .post'));
-
-              if (node.matches('.book, .post')) {
-                newTargets.push(node);
-              }
-
+            const newTargets = node.querySelectorAll('.post, .book');
+            if (newTargets.length > 0) {
               observeTargets(newTargets);
-            }, 200);
+            }
           }
         });
       }
