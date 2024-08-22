dff73d1f9438e8a82b6cfc8111bafaa7fdb31596
bluedream74 <bluedream7004@gmail.com>
2024-08-22 14:16:59 +0000

    fix: login api response

--- viewy/accounts/views.py
+++ viewy/accounts/views.py
@@ -1036,10 +1036,10 @@ class LoginAPIView(APIView):
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
-        print(email)
+        print(password)
         user = authenticate(request=request, username=email, password=password)
         if user is not None:
             login(request, user)
-            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            return Response(True, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(False, status=status.HTTP_400_BAD_REQUEST)
