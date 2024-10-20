0e08debd15deee980e2073ee1138cc557536c213
Ubuntu <ubuntu@ip-172-31-34-177.ap-northeast-1.compute.internal>
2024-10-20 14:27:07 +0000

    celeryファイルの変更

--- viewy/viewy/celery.py
+++ viewy/viewy/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from django.conf import settings
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewy.settings.local')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewy.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewy.settings')
 
 app = Celery('viewy')
 
